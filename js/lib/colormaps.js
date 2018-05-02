71628165fed24dffa19ae1c60b8e828d583becb5
Madicken Munk <madicken.munk@gmail.com>
2018-05-02 16:51:42 -0500

    WIP: figuring out serialization

--- js/lib/colormaps.js
+++ js/lib/colormaps.js
@@ -1,32 +1,39 @@
 var widgets = require('@jupyter-widgets/base');
 var ipydatawidgets = require('jupyter-dataserializers');
 var yt_tools = require('yt-tools');
-var mpl_cmap_obj = {}
 
 var CMapModel = widgets.WidgetModel.extend({
 
     defaults: function() {
-        },
+        return _.extend(widgets.WidgetModel.prototype.defaults.call(this), {
+            _model_name: 'CMapModel',
+            _model_module: 'yt-jscanvas',
+            _model_module_version: '0.1.0'
+        });
+    },
 
     initialize: function() {
         widgets.WidgetModel.prototype.initialize.apply(this);
 
         console.log('initializing colormaps object in WASM')
         this.colormaps = yt_tools.Colormaps.new();
-        this.add_mpl_colormaps();
+        this.add_mpl_colormaps_to_wasm();
 
     },
 
-    add_mpl_colormaps: function() {
+    add_mpl_colormaps_to_wasm: function() {
         // initializes the colormaps module from yt tools and adds the 
         // arrays stored in the self.cmaps dict on the python side into
         // the colormaps object in wasm.
-        //
+        
+        var mpl_cmap_obj = this.get('cmaps');
         var mapname = '';
-        var maptable;
         for (mapname in mpl_cmap_obj) {
-            console.log('adding in colormap of size ')
-            this.colormaps.add_cmap(mapname, mpl_cmap_obj)
+            console.log('adding in colormap');
+            // getArray is used to access something passed with NDArrayWidget. 
+            var maptable = ipydatawidgets.getArray(somethingmapname);
+            console.log(maptable);
+            this.colormaps.add_cmap(mapname, maptable)
         },
     }, 
 }, {
--- yt_pycanvas/colormaps/colormaps.py
+++ yt_pycanvas/colormaps/colormaps.py
@@ -1,16 +1,28 @@
 import ipywidgets as ipywidgets
+from ipydatawidgets import DataUnion, shape_constraints, \
+        data_union_serialization
 import numpy as np
+import traitlets
 
 @ipywidgets.register
 class ColorMaps(ipywidgets.Widget):
+    """Colormapping widget used to collect available colormaps """
 
-    """Colormapping widget used for rendering views """
+    _model_name = traitlets.Unicode('CMapModel').tag(sync=True)
+    _model_module = traitlets.Unicode('yt-jscanvas').tag(sync=True)
+    _model_module_version = traitlets.Unicode('^0.1.0').tag(sync=True)
+    # note: maybe sync=True is unnecessary here since this isn't directly sent
+    # to the browser?
 
     def __init__(self):
         print("getting colormaps from matplotlib...")
 
-        self.cmaps = {}
+        super(ColorMaps, self).__init__()
+        self.cmaps = self.get_mpl_cmaps()
 
+    def get_mpl_cmaps(self):
+        """ Adds available colormaps from matplotlib."""
+        cmaps = {}
         try:
             import matplotlib.cm as mplcm
         except ImportError:
@@ -20,7 +32,6 @@ class ColorMaps(ipywidgets.Widget):
             for colormap in cmap_list:
                 cmap = mplcm.get_cmap(colormap)
                 vals = cmap(np.mgrid[0.0:1.0:256j])
-                self.cmaps[colormap] = vals
-
-        super(ColorMaps, self).__init__()
-
+                cmaps[colormap] = vals
+        # maybe trying to pass the Dict of arrays will work? Not sure yet.
+        return traitlets.Dict(cmaps).tag(sync=True)
