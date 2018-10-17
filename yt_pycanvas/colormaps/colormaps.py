89b51ee1a71378ee58889d0cf446bf8208c192f2
Matthew Turk <matthewturk@gmail.com>
2018-10-17 15:11:22 -0500

    removing print statement, removing catch for mpl

--- yt_pycanvas/colormaps/colormaps.py
+++ yt_pycanvas/colormaps/colormaps.py
@@ -24,25 +24,19 @@ class ColorMaps(ipywidgets.Widget):
     generation = traitlets.Int(0).tag(sync=True, config=True)
 
     def __init__(self, *args, **kwargs):
-        print("getting colormaps from matplotlib...")
-
         self.cmaps = self.get_mpl_cmaps()
         super(ColorMaps, self).__init__()
 
     def get_mpl_cmaps(self):
         """ Adds available colormaps from matplotlib."""
         colormaps = {}
-        try:
-            import matplotlib.cm as mplcm
-        except ImportError:
-            print("can't import matplotlib.cm")
-        else:
-            cmap_list =  mplcm.cmap_d.keys()
-            for colormap in cmap_list:
-                cmap = mplcm.get_cmap(colormap)
-                vals = (cmap(np.mgrid[0.0:1.0:256j])*255).astype("uint8")
-                # right now let's just flatten the arrays. Later we can
-                # serialize each cmap on its own.
-                table = vals.flatten().tolist()
-                colormaps[colormap] = table
+        import matplotlib.cm as mplcm
+        cmap_list =  mplcm.cmap_d.keys()
+        for colormap in cmap_list:
+            cmap = mplcm.get_cmap(colormap)
+            vals = (cmap(np.mgrid[0.0:1.0:256j])*255).astype("uint8")
+            # right now let's just flatten the arrays. Later we can
+            # serialize each cmap on its own.
+            table = vals.flatten().tolist()
+            colormaps[colormap] = table
         return colormaps
