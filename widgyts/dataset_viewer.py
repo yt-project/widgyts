3e5ff1900cb3024678b1a29d330937d45adb909c
xiaotong <xh9@illinois.edu>
2021-11-01 22:31:33 -0500

    Set the current position of the camera to the position that is selected in the listbox.

--- widgyts/dataset_viewer.py
+++ widgyts/dataset_viewer.py
@@ -213,9 +213,9 @@ class AMRDomainViewer(DomainViewer):
         select = ipywidgets.Select(options=[], description="Positions:", disabled=False)
 
         def on_selection_changed(change):
-            self.renderer.camera.position = self.position_list[change["new"]]
+            self.renderer.camera.position = tuple(change["new"])
 
-        select.observe(on_selection_changed, ["index"])
+        select.observe(on_selection_changed, ["value"])
 
         return ipywidgets.HBox(
             [
