77b0392a21c9ed7eef5a98c2a312c1db935b8737
Matthew Turk <matthewturk@gmail.com>
2022-12-30 10:39:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,62 +1,94 @@
 """
 widgyts setup
 """
+import json
+import sys
 from pathlib import Path
 
 import setuptools
-from jupyter_packaging import (
-    combine_commands,
-    create_cmdclass,
-    ensure_targets,
-    install_npm,
-    skip_if_exists,
-)
 
 HERE = Path(__file__).parent.resolve()
 
 # The name of the project
 name = "widgyts"
 
-lab_path = HERE / name / "labextension"
+lab_path = HERE / name.replace("-", "_") / "labextension"
 
 # Representative files that should exist after a successful build
-jstargets = [
-    str(lab_path / "package.json"),
-]
-
-package_data_spec = {
-    name: ["*"],
-}
+ensured_targets = [str(lab_path / "package.json"), str(lab_path / "static/style.js")]
 
 labext_name = "@yt-project/yt-widgets"
 
 data_files_spec = [
-    (f"share/jupyter/labextensions/{labext_name}", str(lab_path), "**"),
-    (f"share/jupyter/labextensions/{labext_name}", str(HERE), "install.json"),
-    ("etc/jupyter/jupyter_server_config.d", "jupyter-config", "widgyts.json"),
+    (
+        f"share/jupyter/labextensions/{labext_name}",
+        str(lab_path.relative_to(HERE)),
+        "**",
+    ),
+    (f"share/jupyter/labextensions/{labext_name}", ".", "install.json"),
+    (
+        "etc/jupyter/jupyter_server_config.d",
+        "jupyter-config/server-config",
+        f"{name}.json",
+    ),
+    # For backward compatibility with notebook server
+    (
+        "etc/jupyter/jupyter_notebook_config.d",
+        "jupyter-config/nb-config",
+        f"{name}.json",
+    ),
 ]
 
-cmdclass = create_cmdclass(
-    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
-)
-
-js_command = combine_commands(
-    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets(jstargets),
-)
-
-is_repo = (HERE / ".git").exists()
-if is_repo:
-    cmdclass["jsdeps"] = js_command
-else:
-    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
+long_description = (HERE / "README.md").read_text()
 
 # Get the package info from package.json
+pkg_json = json.loads((HERE / "package.json").read_bytes())
 
 setup_args = dict(
-    cmdclass=cmdclass,
+    name=name,
+    version=pkg_json["version"],
+    url=pkg_json["homepage"],
+    author=pkg_json["author"]["name"],
+    author_email=pkg_json["author"]["email"],
+    description=pkg_json["description"],
+    license=pkg_json["license"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    include_package_data=True,
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Jupyter",
+    ],
 )
 
+try:
+    from jupyter_packaging import get_data_files, npm_builder, wrap_installers
+
+    post_develop = npm_builder(
+        build_cmd="install:extension", source_dir="src", build_dir=lab_path
+    )
+    setup_args["cmdclass"] = wrap_installers(
+        post_develop=post_develop, ensured_targets=ensured_targets
+    )
+    setup_args["data_files"] = get_data_files(data_files_spec)
+except ImportError as e:
+    import logging
+
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+    logging.warning(
+        "Build tool `jupyter-packaging` is missing. Install it with pip or conda."
+    )
+    if not ("--name" in sys.argv or "--version" in sys.argv):
+        raise e
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
