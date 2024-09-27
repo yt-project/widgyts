49da0eb86ce430207e0c8a69ef9782a41e580b9b
chavlin <chris.havlin@gmail.com>
2024-09-27 14:02:30 -0500

    add back eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,17 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': ['PascalCase'],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
     '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
